8d05ea51eb0911a25a0ada9ecd879d8751d98895
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-13 07:27:00 +0100

    first day

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+
+get '/' do 
+  'hello world'
+end
+
+get '/secret' do
+  'its a secret'
+end
+
+get '/me' do
+  'hello sweetie'
+end
+
+get'/random-cat' do
+  @name = ['Amigo','Tiago', 'Abdur'].sample
+  erb(:index) 
+end
+
+get'/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index) 
+end
